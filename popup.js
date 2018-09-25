a10c7ab5be120bd9d2fbe9092f28a95162e117bb
moswen <moswen@meijiabang.cn>
2018-09-25 18:29:05 +0800

    style(popup.js): 去掉注释

--- popup.js
+++ popup.js
@@ -1,12 +1,5 @@
-// vim: set et sw=4 ts=4 sts=4 ft=javascript fdm=marker ff=unix fenc=utf8 nobomb:
-/**
- * @author mingcheng<lucky#gracecode.com>
- * @date   2013-11-15
- */
-
 Zepto(function($) {
   chrome.tabs.getSelected(null, function(tab) {
-    //var url = "http://c.wotula.com/chart.php?size=32&level=H&val=" + encodeURIComponent(tab.url);
     if (tab.favIconUrl) {
       $("#fav")
         .attr("src", tab.favIconUrl)
@@ -27,6 +20,5 @@ Zepto(function($) {
       canvas.style.boxShadow = '0 0 2px rgba(0, 0, 0, .3)'
       document.getElementById("qr").appendChild(canvas);
     });
-    //$("#qr").attr("src", url);
   });
 });
